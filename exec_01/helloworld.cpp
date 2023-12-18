b890a12931f35a01eaa04fb0b6257baff74948e3
feigu1 <2689564594@qq.com>
2023-12-18 19:42:35 +0800

    exec_01 commit

--- exec_01/helloworld.cpp
+++ exec_01/helloworld.cpp
@@ -2,6 +2,8 @@
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "helloworld";
+    std::cout << "come up and c++ me some time.";
+    std::cout << std::endl;
+    std::cout << "you won't regret it!" << std::endl;
     return 0;
 }
