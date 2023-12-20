2a9a112e44ced2a0af5abd551417c19494b015fd
feigu1 <2689564594@qq.com>
2023-12-20 15:21:58 +0800

    learn_03 commit

---
+++ learn_03/build/CMakeFiles/exceed.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exceed.dir/src/exceed.cpp.o"
+  "CMakeFiles/exceed.dir/src/exceed.cpp.o.d"
+  "exceed"
+  "exceed.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exceed.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ learn_03/build/CMakeFiles/limits.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/limits.dir/src/limits.cpp.o"
+  "CMakeFiles/limits.dir/src/limits.cpp.o.d"
+  "limits"
+  "limits.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/limits.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
